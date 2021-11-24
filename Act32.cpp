0f108007ed861a8f8b6664fc23246de924b59484
Gurtubay <44202873+Gurtubay@users.noreply.github.com>
2021-11-23 23:04:25 -0600

    Add files via upload

--- Act32.cpp
+++ Act32.cpp
@@ -4,22 +4,43 @@
 #include "ListaDoble.hpp"
 #include "ListaDobleCircular.hpp"
 
+using namespace std;
+
 int main(){
     //Leemos el archivo
     ifstream datos;
     datos.open("bitacoraPrueba.txt");
-    string Ip, mes, dia, hora, falla;
+    string Ipp, mes, dia, hora, falla;
+    ListaDoble<Ip*>* AccesosIp=new ListaDoble<Ip*>();
     ListaDoble<string> * listaIps= new ListaDoble<string>();
     while(datos.good()){
         getline(datos,mes,' ');
         getline(datos,dia,' ');
         getline(datos,hora,' ');
-        getline(datos,Ip,' ');
+        getline(datos,Ipp,' ');
         getline(datos,falla);
-        if(!(listaIps->buscarNodoT(Ip)))
-            listaIps->agregarFinal(Ip);
-            
-    }
+
+        if(listaIps->buscarNodoT(Ipp)){
+            NodoD<Ip*> * nodo=AccesosIp->getHead();
+            while(nodo){
+                if(Ipp==nodo->getDato()->getDirIp())
+                    nodo->getDato()->agregarAcceso();
+                nodo=nodo->getSiguiente();
+        }}
+
+        else{
+            listaIps->agregarFinal(Ipp);
+            AccesosIp->agregarFinal(new Ip(Ipp));
+        }}
+
+    NodoD<Ip*> * prueba=AccesosIp->getHead();
+    do
+    {
+        cout<<prueba->getDato()->getDirIp()<<endl;
+        cout<<prueba->getDato()->getNumAccesos()<<endl;
+        prueba=prueba->getSiguiente();
+    } while (AccesosIp->getHead()->getAnterior()!=prueba);
+
 
     return 0;
 }
