948861ea84adc151f8d6d23f147b60ca93be1cb2
Gurtubay <44202873+Gurtubay@users.noreply.github.com>
2021-11-24 00:03:10 -0600

    Add files via upload

--- Acceso.hpp
+++ Acceso.hpp
@@ -1,7 +1,32 @@
+#ifndef ABC
+#define ABC
+
 #include <vector>
+#include "Ip.hpp"
+#include <string>
+
+using namespace std;
 
 class Acceso{
-    int numAccesos;
-    vector<string> nomIp;
+    private:
+        int numAccesos;
+        vector<string> * nomIps; //Posiblemente se apunta
+    public:
+        Acceso(int numAccesos){
+            this->numAccesos=numAccesos;
+            this->nomIps= new vector<string>;//()
+        }
 
+        //Getter y Setters
+        int getAccesos(){return this->numAccesos;}
+        void setAccesos(int Accesos){this->numAccesos=Accesos;}
+        vector<string> * getIps(){return this->nomIps;}
+        void setIps(vector<string> * Ips){this->nomIps=Ips;}
+
+        //Esto chance se borra
+        void agregarIp(string nuevaIp){
+            this->nomIps->push_back(nuevaIp);
+        }
 };
+
+#endif
--- Act32.cpp
+++ Act32.cpp
@@ -1,6 +1,7 @@
 #include "BST.hpp"
 #include <fstream>
 #include "Ip.hpp"
+#include "Acceso.hpp"
 #include "ListaDoble.hpp"
 #include "ListaDobleCircular.hpp"
 
@@ -11,6 +12,7 @@ int main(){
     ifstream datos;
     datos.open("bitacoraPrueba.txt");
     string Ipp, mes, dia, hora, falla;
+    ListaDoble<Acceso *> * ListaAccesos=new ListaDoble<Acceso *>();
     ListaDoble<Ip*>* AccesosIp=new ListaDoble<Ip*>();
     ListaDoble<string> * listaIps= new ListaDoble<string>();
     while(datos.good()){
@@ -32,14 +34,44 @@ int main(){
             listaIps->agregarFinal(Ipp);
             AccesosIp->agregarFinal(new Ip(Ipp));
         }}
+    datos.close();
 
-    NodoD<Ip*> * prueba=AccesosIp->getHead();
+    /*NodoD<Ip*> * prueba=AccesosIp->getHead();
     do
     {
         cout<<prueba->getDato()->getDirIp()<<endl;
         cout<<prueba->getDato()->getNumAccesos()<<endl;
         prueba=prueba->getSiguiente();
     } while (AccesosIp->getHead()->getAnterior()!=prueba);
+    */
+
+    NodoD<Ip*> * nodo2=AccesosIp->getHead();
+    NodoD<Acceso *> * nuevoNodo = ListaAccesos->getHead();
+    while(nodo2){
+        if(ListaAccesos){
+            while (ListaAccesos!=nullptr)
+            {
+                if(nuevoNodo->getDato()->getAccesos()==nodo2->getDato()->getNumAccesos()){
+                    nuevoNodo->getDato()->agregarIp(nodo2->getDato()->getDirIp());
+                    nodo2=nodo2->getSiguiente();
+                    nuevoNodo=nuevoNodo->getSiguiente();
+                }
+            }
+            
+        }
+        else{
+            int num_accesos = nodo2->getDato()->getNumAccesos();
+            Acceso * nuevoObj = new Acceso(num_accesos);
+            ListaAccesos->agregarFinal(nuevoObj);
+            
+        }
+            
+    }
+   //BST<int> * arbol= new BST;<int>();
+    //Agregamos los nodos al arbol con el ciclo for
+    /*for(int dato: ){
+        arbol->agregarNodo(dato);
+    }*/
 
 
     return 0;
